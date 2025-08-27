255f908f7e982409572be3fa1f6c2c89046feb3a
Naz <doctor.n4z@gmail.com>
2025-08-27 00:09:06 -0400

    babel setup

---
+++ reusable-react-components/index.js
@@ -0,0 +1,19 @@
+// Armor
+export { default as ArmsArmorSlotIcon } from './armor/arms/ArmsArmorSlotIcon.jsx';
+export { default as ChestArmorSlotIcon } from './armor/chest/ChestArmorSlotIcon.jsx';
+export { default as HelmArmorSlotIcon } from './armor/helm/HelmArmorSlotIcon.jsx';
+export { default as LegsArmorSlotIcon } from './armor/legs/LegsArmorSlotIcon.jsx';
+export { default as TorsoArmorSlotIcon } from './armor/torso/TorsoArmorSlotIcon.jsx';
+
+// Weapons
+export { default as BowIcon } from './weapons/Bow/BowIcon.jsx';
+export { default as ChargeBladeIcon } from './weapons/ChargeBlade/ChargeBladeIcon.jsx';
+export { default as DualBladesIcon } from './weapons/DualBlades/DualBladesIcon.jsx';
+export { default as GreatSwordIcon } from './weapons/GreatSword/GreatSwordIcon.jsx';
+export { default as GunlanceIcon } from './weapons/Gunlance/GunlanceIcon.jsx';
+export { default as HammerIcon } from './weapons/Hammer/HammerIcon.jsx';
+export { default as LanceIcon } from './weapons/Lance/LanceIcon.jsx';
+export { default as LightBowgunIcon } from './weapons/LightBowgun/LightBowgunIcon.jsx';
+export { default as LongSwordIcon } from './weapons/LongSword/LongSwordIcon.jsx';
+export { default as SwitchAxeIcon } from './weapons/SwitchAxe/SwitchAxeIcon.jsx';
+export { default as SwordAndShieldIcon } from './weapons/SwordAndShield/SwordAndShieldIcon.jsx';
