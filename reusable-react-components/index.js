1d891528c0f9514da68a6e283744531168290cc1
Naz <doctor.n4z@gmail.com>
2025-08-27 00:43:33 -0400

    updated dist index.js

--- reusable-react-components/index.js
+++ reusable-react-components/index.js
@@ -1,19 +1,20 @@
+
 // Armor
-export { default as ArmsArmorSlotIcon } from './armor/ArmsArmorSlotIcon.jsx';
-export { default as ChestArmorSlotIcon } from './armor/ChestArmorSlotIcon.jsx';
-export { default as HelmArmorSlotIcon } from './armor/HelmArmorSlotIcon.jsx';
-export { default as LegsArmorSlotIcon } from './armor/LegsArmorSlotIcon.jsx';
-export { default as TorsoArmorSlotIcon } from './armor/TorsoArmorSlotIcon.jsx';
+export { default as ArmsArmorSlotIcon } from './armor/ArmsArmorSlotIcon';
+export { default as ChestArmorSlotIcon } from './armor/ChestArmorSlotIcon';
+export { default as HelmArmorSlotIcon } from './armor/HelmArmorSlotIcon';
+export { default as LegsArmorSlotIcon } from './armor/LegsArmorSlotIcon';
+export { default as TorsoArmorSlotIcon } from './armor/TorsoArmorSlotIcon';
 
 // Weapons
-export { default as BowIcon } from './weapons/Bow/BowIcon.jsx';
-export { default as ChargeBladeIcon } from './weapons/ChargeBlade/ChargeBladeIcon.jsx';
-export { default as DualBladesIcon } from './weapons/DualBlades/DualBladesIcon.jsx';
-export { default as GreatSwordIcon } from './weapons/GreatSword/GreatSwordIcon.jsx';
-export { default as GunlanceIcon } from './weapons/Gunlance/GunlanceIcon.jsx';
-export { default as HammerIcon } from './weapons/Hammer/HammerIcon.jsx';
-export { default as LanceIcon } from './weapons/Lance/LanceIcon.jsx';
-export { default as LightBowgunIcon } from './weapons/LightBowgun/LightBowgunIcon.jsx';
-export { default as LongSwordIcon } from './weapons/LongSword/LongSwordIcon.jsx';
-export { default as SwitchAxeIcon } from './weapons/SwitchAxe/SwitchAxeIcon.jsx';
-export { default as SwordAndShieldIcon } from './weapons/SwordAndShield/SwordAndShieldIcon.jsx';
+export { default as BowIcon } from './weapons/Bow/BowIcon';
+export { default as ChargeBladeIcon } from './weapons/ChargeBlade/ChargeBladeIcon';
+export { default as DualBladesIcon } from './weapons/DualBlades/DualBladesIcon';
+export { default as GreatSwordIcon } from './weapons/GreatSword/GreatSwordIcon';
+export { default as GunlanceIcon } from './weapons/Gunlance/GunlanceIcon';
+export { default as HammerIcon } from './weapons/Hammer/HammerIcon';
+export { default as LanceIcon } from './weapons/Lance/LanceIcon';
+export { default as LightBowgunIcon } from './weapons/LightBowgun/LightBowgunIcon';
+export { default as LongSwordIcon } from './weapons/LongSword/LongSwordIcon';
+export { default as SwitchAxeIcon } from './weapons/SwitchAxe/SwitchAxeIcon';
+export { default as SwordAndShieldIcon } from './weapons/SwordAndShield/SwordAndShieldIcon';
